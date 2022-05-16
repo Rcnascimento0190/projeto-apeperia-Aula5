63c562419fbba4a1b087df7b25efe7a9a384572c
Rodrigo <rcnascimento0190@gmail.com>
2022-05-16 15:31:30 -0300

    Criando arquivo index.html com lista de cursos

---
+++ logica02/Golden_aritimetic_progretion.py
@@ -0,0 +1,19 @@
+Golden Sachs interviw question 
+
+def solution(N):
+    count = 0 
+    n = 2
+     while 2 * N + n ** 2 > 0: 
+        a = (2 * N + n - n ** 2) / (2 * n)
+
+        if a - int (a) == 0
+        print( a, n)
+        count + = 1
+
+        n += 1
+
+        return count
+
+        print(' The number of way to return reach 100 is %d'solution(100))
+        
+
